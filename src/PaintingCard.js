d57d01e09c5832fcb92bf89ea1f39c97df471f08
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-12-03 14:35:15 -0800

    created paintingcard.js

---
+++ src/PaintingCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PaintingCard({painting, deletePainting}){
+
+    return (
+        <div>
+            <h1>Title: {painting.title}</h1>
+            <h3>Year Created: {painting.year_created}</h3>
+            <h4>Artist ID: {painting.artist_id}</h4>
+            <button className="delete-button" onClick={() => deletePainting(painting.id)}> Delete </button>
+        </div>
+    )
+}
+
+export default PaintingCard
